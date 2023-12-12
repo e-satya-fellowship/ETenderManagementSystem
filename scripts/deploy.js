92fa54f208149a7e6f39d932a082fb37620ff72f
romanshilpakar <iamromanshilpakar@gmail.com>
2023-12-12 22:22:18 +0545

    E-TenderManagementSystem

---
+++ scripts/deploy.js
@@ -0,0 +1,41 @@
+const path = require("path");
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    const tenderManagementSystem = await ethers.deployContract("TenderManagementSystem");
+    const contract_address = await tenderManagementSystem.getAddress()
+    console.log("Contract address:",contract_address)
+    saveFrontendFiles(contract_address);
+
+  }
+
+   function saveFrontendFiles(contract_address) {
+    const fs = require("fs");
+    const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts");
+  
+    if (!fs.existsSync(contractsDir)) {
+      fs.mkdirSync(contractsDir);
+    }
+  
+    fs.writeFileSync(
+      path.join(contractsDir, "contract-address.json"),
+      JSON.stringify({ TenderManagementSystem: contract_address }, undefined, 2)
+    );
+  
+    const TenderManagementSystemArtifact = artifacts.readArtifactSync("TenderManagementSystem");
+  
+    fs.writeFileSync(
+      path.join(contractsDir, "TenderManagementSystem.json"),
+      JSON.stringify(TenderManagementSystemArtifact, null, 2)
+    );
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
